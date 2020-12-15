8bd8e5e96341817c83bbb2a12951d6181c3e0df1
Airy-Dev6 <airysilva11@gmail.com>
2020-12-14 18:10:45 -0600

    listo 1.0

--- js/main.js
+++ js/main.js
@@ -50,6 +50,55 @@ const printPosts = posts => {
         $(".container-posts").append(entryHTML)
     }
 }
+const printPostsReading = posts => {
+    $(".cardsEmpy").empty()
+    for ( key in posts) {
+        /* console.log("key", key)
+        console.log("object ", posts[key]) */
+        let {title, tags} = posts[key]
+        let entryHTML = `
+        <div class="card mb-lg-3 mb-md-0 mb-sm-0 "data-entry-key=${key} >
+            <div class="card-body print-card" >
+                <div class="footer-card-central">
+                    <img src="assets/images/profile-2.jpg" alt="" class="author-profile">
+                    <div class="footer-firm">
+                        <p class="card-text">Mateusz Leciejewski</p>
+                        <a class="card-text">Nov 9 (5 hours ago)</a>
+                    </div>
+                </div>
+                <div class="card-title print-card" onclick="saveKeyModal('${key}')" data-toggle="modal" data-target="#modal-card">
+                    <h2>${title}</h2>
+                </div>
+                <div class="tag-link">
+                    <a><span></span>${tags}</a>
+                </div>
+                <div class="reactions">
+                    <div class="comments">
+                        <a href=""><span><svg class="crayons-icon" width="24" height="24"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M18.884 12.595l.01.011L12 19.5l-6.894-6.894.01-.01A4.875 4.875 0 0112 5.73a4.875 4.875 0 016.884 6.865zM6.431 7.037a3.375 3.375 0 000 4.773L12 17.38l5.569-5.569a3.375 3.375 0 10-4.773-4.773L9.613 10.22l-1.06-1.062 2.371-2.372a3.375 3.375 0 00-4.492.25v.001z">
+                                    </path>
+                                </svg></span>13 reactions</a>
+                        <a href=""><span><svg class="crayons-icon" width="24" height="24"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M10.5 5h3a6 6 0 110 12v2.625c-3.75-1.5-9-3.75-9-8.625a6 6 0 016-6zM12 15.5h1.5a4.501 4.501 0 001.722-8.657A4.5 4.5 0 0013.5 6.5h-3A4.5 4.5 0 006 11c0 2.707 1.846 4.475 6 6.36V15.5z">
+                                    </path>
+                                </svg></span>3 comments</a>
+                    </div>
+                    <div class="time-read">
+                        <p>2 min read</p>
+                        <button class="btn-read-save button-save" onclick="saveKey('${key}')" type="button" data-entry-key=${key}>Save</button>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+        `
+        $(".cardsEmpy").append(entryHTML)
+    }
+}
 /* $(document).click(event => {
     console.log(event.target)
 }) */
@@ -63,8 +112,8 @@ let saveKey = key => {
 $(".reading-list").click(event => {
     event.preventDefault()
     console.log("hola")
-    $(".container-posts").empty()
-    printPosts(selectedPost)
+    $(".cardsEmpy").empty()
+    printPostsReading(selectedPost)
 })
 
 let saveKeyModal = key => {
@@ -159,7 +208,9 @@ $(document).on('keyup','#entry',event=> {
         if(title.toLowerCase().includes(search.toLowerCase())) {
             searchResultObject[key] = allPosts[key]
             //searchResultObject = {...searchResultObject, post}
-            printPosts(searchResultObject)
+            $(".cardsEmpy").empty()
+            printPostsReading(searchResultObject)
+            /* printPosts(searchResultObject) */
         }
 
 
